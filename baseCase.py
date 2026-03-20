b1f712fd00b9a4ce78b273e6c28e9962b290582a
Keshav Choudhary <keshav7aug@gmail.com>
2026-03-20 15:06:22 +0530

    updated input to chat template

--- baseCase.py
+++ baseCase.py
@@ -5,16 +5,25 @@ from transformers import AutoConfig, AutoModelForCausalLM
 from huggingface_hub import snapshot_download
 import torch.nn as nn
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-device = "cuda" if torch.cuda.is_available() else "cpu"
-tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(
-    model_id,
-    trust_remote_code=True
-)
-prompt = 'Where is Delhi'
-inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
+import argparse
 
-outputs = model.generate(**inputs)
-text = tokenizer.batch_decode(outputs)
-print(text)
+def run(user):
+    model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        trust_remote_code=True
+    )
+    user = "Where is Delhi"
+    prompt = f"<|system|>\nYou are a helpful assistant.<|user|>\n{user}<|assistant|>\n"
+    inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
+
+    outputs = model.generate(**inputs)
+    text = tokenizer.batch_decode(outputs)
+    return text
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prompt", type=str, default="Where is Delhi")
+    print(run(parser.parse_args().prompt))
